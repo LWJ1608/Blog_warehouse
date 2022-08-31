400610a9bad19ac799532b0933447c7df10809bf
LWJ <2194677100@qq.com>
2022-08-31 17:07:39 +0800

    2022/08/31 周三 17:07:39.47

--- git_push.bat
+++ git_push.bat
@@ -1,20 +1,20 @@
 @echo off
 echo "DOCS PUSH BAT"
 
-echo "1. Move to working directory" 
+echo "------------------1. Move to working directory-------------------------------------------" 
 cd E:\桌面\专业学习\Linux_C_C++\Blog_warehouse
 
-git pull
-echo "2. Start submitting code to the local repository"
+git pull 
+echo "------------------2. Start submitting code to the local repository------------------------"
 git add .
  
-echo "3. Commit the changes to the local repository"
+echo "------------------3. Commit the changes to the local repository--------------------------"
 set now=%date% %time%
 echo "Time:" %now%
 git commit -m "%now%"
  
-echo "4. Push the changes to the remote git server"
+echo "------------------4. Push the changes to the remote git server----------------------------"
 git push origin master
  
-echo "Batch execution complete!"
+echo "Batch execution complete!!!!!!!!!!"
 pause
