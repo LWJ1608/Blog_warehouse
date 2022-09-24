e4cd45df0d24fa3b0ce34ad29874b694361fbc55
LWJ <2194677100@qq.com>
2022-09-24 16:19:09 +0800

    2022/09/24 周六 16:19:09.08

--- git_push.bat
+++ git_push.bat
@@ -4,7 +4,8 @@ echo "DOCS PUSH BAT"
 echo "------------------1. Move to working directory-------------------------------" 
 cd E:\桌面\专业学习\Linux_C_C++\Blog_warehouse
 
-git pull origin master
+git pull 
+
 echo "------------------2. Start submitting code to the local repository------------"
 git add .
  
